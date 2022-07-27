cf859fcb871b753d3dd47354427da1c518d38d97
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2022-07-27 17:54:39 +0300

    .
    ..

---
+++ basvuruForm/basvuruForm/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using basvuruForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basvuruForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public ViewResult EgitimDurumu()
+        {
+            return View();
+        }
+
+        public ViewResult IsTecrubesi()
+        {
+            return View();
+        }
+
+        public ViewResult LastPage()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
